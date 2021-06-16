0f8efba2b12826b42993ade5b0855e3f8e8b999c
Meni Nuriel <nurielmeni@gmail.com>
2021-06-16 15:31:16 +0300

    Fix name

--- views/socialConnect.php
+++ views/socialConnect.php
@@ -1,9 +1,9 @@
 <section id="<?= empty($name) ? 'social-icons' : $name ?>" class="social-icons <?= $cssClass ?>">
 	<ul>
-		<?php foreach ($items as $item) : ?>
+		<?php foreach ($items as $name => $item) : ?>
 			<li>
 				<a class="dt-icons-selector" href="<?= $itme['url'] ?>" rel="noopener" target="_blank">
-					<i class="dt-icon-<?= $itme['social'] ?>"></i>
+					<i class="dt-icon-<?= $name ?>"></i>
 				</a>
 			</li>
 		<?php endforeach; ?>
