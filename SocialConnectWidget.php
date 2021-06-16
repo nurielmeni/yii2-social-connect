42109665b83ac99115cbb8419a109893645c47ab
Meni Nuriel <nurielmeni@gmail.com>
2021-06-16 16:09:01 +0300

    Link Class

--- SocialConnectWidget.php
+++ SocialConnectWidget.php
@@ -12,6 +12,7 @@ class SocialConnectWidget extends Widget
 
     public $name = 'social-connect';
     public $cssClass = '';
+    public $linkClass = '';
     public $size = '';
     public $color = '';
     public $items = [];
@@ -27,6 +28,7 @@ class SocialConnectWidget extends Widget
         return $this->render('socialConnect', [
             'name' => $this->name,
             'cssClass' => $this->cssClass,
+            'linkClass' => $this->linkClass,
             'size' => $this->size,
             'color' => $this->color,
             'items' => $this->items
