9b2713570483e719e8d8b1ef54a5a58a36e926ea
Meni Nuriel <nurielmeni@gmail.com>
2021-06-16 15:47:17 +0300

    Typo

--- views/socialConnect.php
+++ views/socialConnect.php
@@ -2,7 +2,7 @@
 	<ul>
 		<?php foreach ($items as $social => $item) : ?>
 			<li>
-				<a class="dt-icons-selector" href="<?= $itme['url'] ?>" rel="noopener" target="_blank">
+				<a class="dt-icons-selector" href="<?= $item['url'] ?>" rel="noopener" target="_blank">
 					<i class="dt-icon-<?= $social ?>"></i>
 				</a>
 			</li>
