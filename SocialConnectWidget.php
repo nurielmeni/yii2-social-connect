58f14dd4ad041fa1fcf9a16a061793ce99ae3924
Meni Nuriel <nurielmeni@gmail.com>
2021-06-17 13:10:19 +0300

    Translation

--- SocialConnectWidget.php
+++ SocialConnectWidget.php
@@ -11,6 +11,7 @@ class SocialConnectWidget extends Widget
 
 
     public $name = 'social-connect';
+    public $title = '';
     public $cssClass = '';
     public $linkClass = '';
     public $size = '';
@@ -21,12 +22,27 @@ class SocialConnectWidget extends Widget
     {
         parent::init();
         SocialConnectAsset::register(\Yii::$app->view);
+        $this->title = empty($this->title) ? Yii::t('extensions/socialConnect', 'Hello world') : $this->title;
+    }
+
+    public function registerTranslations()
+    {
+        $i18n = Yii::$app->i18n;
+        $i18n->translations['extensions/socialConnect/*'] = [
+            'class' => 'yii\i18n\PhpMessageSource',
+            'sourceLanguage' => 'en-US',
+            'basePath' => '@socialConnect/messages',
+            'fileMap' => [
+                '@socialConnect/messages' => 'messages.php',
+            ],
+        ];
     }
 
     public function run()
     {
         return $this->render('socialConnect', [
             'name' => $this->name,
+            'title' => $this->title,
             'cssClass' => $this->cssClass,
             'linkClass' => $this->linkClass,
             'size' => $this->size,
