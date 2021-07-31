b06d96b45f364ef6ee4b16bda1f522d86201b937
Meni Nuriel <nurielmeni@gmail.com>
2021-07-31 15:12:00 +0300

    s

--- views/socialConnect.php
+++ views/socialConnect.php
@@ -3,7 +3,7 @@
 		<?php foreach ($items as $social => $item) : ?>
 			<li>
 				<a class="dt-icons-selector <?= $linkClass ?>" href="<?= $item['url'] ?>" rel="noopener" title="<?= $title ?>" target="_blank">
-					<?php if (array_key_exists('imgUrl', $items)) : ?>
+					<?php if (array_key_exists('imgUrl', $item)) : ?>
 						<img src="<?= $item['imgUrl'] ?>" width="<?= $size ?>" alt="<?= $social ?> icon" class=" dt-icon-<?= $social ?>">
 					<?php else : ?>
 						<i style="<?= empty($size) ? "" : "font-size: $size; " ?><?= empty($color) ? "" : "color: $color; " ?>" ></i>
